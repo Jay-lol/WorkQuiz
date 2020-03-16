63af63d0f76b2c69016f856eb32e90ade5c7877a
Jay-lol <krgeo@naver.com>
2020-03-17 01:52:04 +0900

    레트로핏 연결 테스트중,어노테이션 정리해야함

--- app/src/main/java/practice/kotlin/com/wordquiz/MainActivity.kt
+++ app/src/main/java/practice/kotlin/com/wordquiz/MainActivity.kt
@@ -3,24 +3,30 @@ package practice.kotlin.com.wordquiz
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     val t = R.string.english_0
     val t1 = t + 1
-    val mQuestions = arrayOf(Question(R.string.english_0, R.string.korean_0), Question(R.string.english_1, R.string.korean_1), Question(R.string.english_2,
-        R.string.korean_2),Question(R.string.english_3, R.string.korean_3), Question
-    (R.string.english_4, R.string.korean_4))
+    val mQuestions = arrayOf(
+        Question(R.string.english_0, R.string.korean_0), Question(R.string.english_1, R.string.korean_1), Question(
+            R.string.english_2,
+            R.string.korean_2
+        ), Question(R.string.english_3, R.string.korean_3), Question
+            (R.string.english_4, R.string.korean_4)
+    )
 
-    private var mCurrentNumber : Int = 0
+    private var mCurrentNumber: Int = 0
     private var mAnswer = mutableListOf<Int>()
 
 
@@ -29,6 +35,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         updateUi()
         setButton()
+        update.setOnClickListener {
+            val retrofitService = getService()
+            retrofitService.getUser("h")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                /*.subscribe({ it ->
+                    Log.d("content", it.toString())
+                    Log.d("userIdx ", it.getAsJsonArray("data").get(0).asJsonObject.get("userIdx").asString)
+                    Log.d("name ", it.getAsJsonArray("data").get(0).asJsonObject.get("name").asString)
+                    Log.d("part ", it.getAsJsonArray("data").get(0).asJsonObject.get("part").asString)
+                    Log.d("profileUrl ", it.getAsJsonArray("data").get(0).asJsonObject.get("profileUrl").asString)
+                    Log.d("message", it.getAsJsonPrimitive("message").asString)
+                    Log.d("status", it.getAsJsonPrimitive("status").asString)*/
+                .subscribe({
+                    Log.d("content", it.toString())
+                    Toast.makeText(this, "$it", Toast.LENGTH_LONG).show()
+                    //it.getAsJsonPrimitive("message").asString
+                    //val test = it.getAsJsonArray("data").get(0).asJsonObject.get("name").asString
+                    retrofitService.getUser("s")
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe {
+                            Log.d("content", it.toString())
+                            Toast.makeText(this, "$it", Toast.LENGTH_LONG).show()
+                        }
+                })
+                {
+                    Log.e("Error", it.message)
+                }
+        }
     }
 
     private fun updateUi() {
@@ -37,16 +73,16 @@ class MainActivity : AppCompatActivity() {
         setAnswerButtonText()
     }
 
-    private fun showQuestion(){
+    private fun showQuestion() {
         question_text.setText("${mCurrentNumber + 1}" + ") " + resources.getString(R.string.question_title))
         question_word_text.setText(resources.getString(mQuestions[mCurrentNumber].english))
 
     }
 
-    private fun setButton(){
+    private fun setButton() {
         previous_btn.setOnClickListener {
             mCurrentNumber = mCurrentNumber - 1
-            if(mCurrentNumber < 0 ){
+            if (mCurrentNumber < 0) {
                 mCurrentNumber = mQuestions.size - 1
             }
             showQuestion()
@@ -58,28 +94,28 @@ class MainActivity : AppCompatActivity() {
             updateUi()
         }
         answer_one.setOnClickListener {
-            if(resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(0)].korean))){
+            if (resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(0)].korean))) {
                 Toast.makeText(this, R.string.answer_true, Toast.LENGTH_SHORT).show()
                 showSettingPopup()
             } else
                 Toast.makeText(this, R.string.answer_false, Toast.LENGTH_SHORT).show()
         }
         answer_two.setOnClickListener {
-            if(resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(1)].korean))){
+            if (resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(1)].korean))) {
                 Toast.makeText(this, R.string.answer_true, Toast.LENGTH_SHORT).show()
                 showSettingPopup()
             } else
                 Toast.makeText(this, R.string.answer_false, Toast.LENGTH_SHORT).show()
         }
         answer_three.setOnClickListener {
-            if(resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(2)].korean))){
+            if (resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(2)].korean))) {
                 Toast.makeText(this, R.string.answer_true, Toast.LENGTH_SHORT).show()
                 showSettingPopup()
             } else
                 Toast.makeText(this, R.string.answer_false, Toast.LENGTH_SHORT).show()
         }
         answer_four.setOnClickListener {
-            if(resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(3)].korean))){
+            if (resources.getString(mQuestions[mCurrentNumber].korean) == resources.getString((mQuestions[mAnswer.get(3)].korean))) {
                 Toast.makeText(this, R.string.answer_true, Toast.LENGTH_SHORT).show()
                 showSettingPopup()
             } else
@@ -101,46 +137,47 @@ class MainActivity : AppCompatActivity() {
 
         alertDialog.setView(view)
         alertDialog.show()
-        alertDialog.window?.setLayout(1000,800)
+        alertDialog.window?.setLayout(1000, 800)
     }
 
-    private fun setAnswerData(){
+    private fun setAnswerData() {
         var isDuplicated = false
         mAnswer.clear()
         mAnswer.add(0, -1)
-        mAnswer.add(1,-1)
-        mAnswer.add(2,-1)
-        mAnswer.add(3,-1)
+        mAnswer.add(1, -1)
+        mAnswer.add(2, -1)
+        mAnswer.add(3, -1)
 
         var cnt = 0
         val random = Random()
 
-        var temp : Int
-        while(true){
+        var temp: Int
+        while (true) {
             temp = random.nextInt(mQuestions.size - 1)
-            if(temp == mCurrentNumber){
+            if (temp == mCurrentNumber) {
                 continue
             }
-            for(i in 0..2){
-                if(temp == mAnswer.get(i)){
+            for (i in 0..2) {
+                if (temp == mAnswer.get(i)) {
                     isDuplicated = true
                 }
             }
-            if(isDuplicated){
+            if (isDuplicated) {
                 isDuplicated = false
                 continue
-            } else{
+            } else {
                 mAnswer.set(cnt, temp)
                 cnt++
             }
-            if(cnt > 2) {
+            if (cnt > 2) {
                 break
             }
         }
         mAnswer.set(3, mCurrentNumber)
         Collections.shuffle(mAnswer)
     }
-    private fun setAnswerButtonText(){
+
+    private fun setAnswerButtonText() {
         answer_one.setText(resources.getString(mQuestions[mAnswer.get(0)].korean))
         answer_two.setText(resources.getString(mQuestions[mAnswer.get(1)].korean))
         answer_three.setText(resources.getString(mQuestions[mAnswer.get(2)].korean))
